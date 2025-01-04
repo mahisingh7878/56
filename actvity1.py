1349c2342b788cd3e1021a9221bbaeba4725887d
mahisingh4415 <mahisinghadityasingh1232@gmail.com>
2025-01-04 21:26:00 +0530

    actvity1.py

---
+++ actvity1.py
@@ -0,0 +1,8 @@
+def printno(n):
+    interation=0
+    print("interation for ",n," is",interation)
+    interation+=1
+    print("total no of iteration for",n," is ",interation)
+
+printno(290)
+printno(69)
